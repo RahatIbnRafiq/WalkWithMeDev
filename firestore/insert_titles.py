29eb9f71a08a2a5445a01c87a96c6289062403b6
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2023-10-18 14:13:31 -0400

    added codes to filkter the wikipedia page title to take out names with special characters

--- firestore/insert_titles.py
+++ firestore/insert_titles.py
@@ -7,9 +7,9 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 # Function to extract locations from text
-def extract_locations(text):
+def location_using_ner(text):
     doc = nlp(text)
-    locations = [ent.text for ent in doc.ents if ent.label_ == "GPE"]
+    locations = [ent.text for ent in doc.ents if ent.label_ in ["LOC", "GPE", "ORG", "FAC"]]
     return locations
 
 
@@ -23,31 +23,34 @@ def is_valid_string(s):
     return all(char in allowed_chars for char in s)
 
 
-def read_titles():
+def filter_special_characters():
     x = 0
     f = open("../data/all_wiki_titles.txt")
+    filtered_candidates = set()
     for line in f:
-        # x+= 1
-        # if x > 100:
-        #     break
+        x+=1
         line = line.strip()
         if is_valid_string(line):
             actual_title = " ".join(line.split("_"))
-            locations = extract_locations(actual_title)
-            if len(locations) > 0:
-                print(locations)
-                data.append({'Name':line})
-            else:
-                print(f"{line}  not a location")
-            print("___________________")
+            filtered_candidates.add(actual_title)
+    f.close()
+    
+    with open("../data/filtered_special_characters.txt", "w") as f:
+        filtered_candidates = list(filtered_candidates)
+        for location_candidate in filtered_candidates:
+            f.write(location_candidate+"\n")
+            
 
 # Use a service account.
 # cred = credentials.Certificate('/Users/rahatibnrafiq/startups/WalkWithMeDev/data/walkwithme-2d86e-firebase-adminsdk-3sysz-95b4347461.json')
 # app = firebase_admin.initialize_app(cred)
 # db = firestore.client()
 
-read_titles()
-print(len(data))
+# filter_special_characters()
+# print(len(data))
+
+
+
 # for record in data:
 #     try:
 #         doc_ref = db.collection("titles").document(record['Name'])
